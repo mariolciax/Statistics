b23c377cf378472cd2cb6328f1cb614617ea4950
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2022-04-27 21:42:45 +0200

    Add files via upload

--- plot_empirical_distribution.R
+++ plot_empirical_distribution.R
@@ -1,22 +1,44 @@
 #------------3.1-------------
+#Theoretical with emphirical distribution
+
+mu <- 0
+sigma <- 1
+x <- seq(mu-3*sigma, mu+3*sigma,length.out = 100) # empirical rule 3 sigma rule
+
+plot(ecdf(sample_1))
+lines(x,pnorm(x), xlab = "X", ylab = "density")
+
+
+#sample_2
+
+plot(ecdf(sample_2))
+lines(x,pnorm(x), xlab = "X", ylab = "density")
+
+
+#-------------------Comments
+
 set.seed(1234)
 library(fitdistrplus)
 (sample_1 <- rnorm(20))
 sample_2 <- rnorm(100)
 
+#Emphirical
+
 plot(sort(sample_1, decreasing=F))
-lines(pnorm(1:20), type="l", add=T)
+#lines(pnorm(1:20), type="l", add=T)
+
+
 plotdist(sample_1)
 
 #with function ecdf
 plot(ecdf(sample_1))
-lines(pnorm(seq(-2,2,0.01)), type="l", add=T) #it doesn't work good
-?ecdf
+#lines(pnorm(seq(-2,2,0.01)), type="l", add=T) #it doesn't work good
+#?ecdf
 
-#sample_2
-plot(sort(sample_2, decreasing=F))
-lines(pnorm(0:100), type="l")
-plotdist(sample_2)
 
-#with function ecdf
-plot(ecdf(sample_2))
+?integrate
+
+
+
+
+
