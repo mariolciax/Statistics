83dd65ad3175f6f8fe743ed74fe3641591382904
Mariola Bartosik <56107837+mariolciax@users.noreply.github.com>
2022-05-26 11:46:30 +0200

    Add files via upload

--- lab6.R
+++ lab6.R
@@ -33,7 +33,6 @@ quantile(srednia_boot,c(alpha/2,1-alpha/2))
 
 
 #wariancja
-
 przedzial_war<-c((n-1)*var(waga)/qchisq(1-alpha/2,n-1), (n-1)*var(waga)/qchisq(alpha/2,n-1))
 przedzial_war
 
@@ -66,8 +65,39 @@ quantile(srednia_boot,c(alpha/2,1-alpha/2))
 #testowanie NADAL t.test
 #H_1: mu=270, H_1: mu!=270
 t.test(waga, mu=270,conf.level = 0.9)
+
 #nie mamy podstaw aby odrzucic bo p-value=0.1272 wieksze niz alfa
 #sobie sprawdzimy ze to 270 lezy w przedziale
 t.test(waga, conf.level = 0.9)
 
+t.test(waga,mu=270,conf.level = 0.9, alternative = "less")
+#odrzucamy hipoteze zerowa na rzecz alternatywy, wartosc p-value to 1/2 poprzedniej
 
+
+#uwaga na jednostronne
+#trzeba miec uzasadnienie
+
+
+#----zad 3.10 kwiatki
+data(iris)
+iris$Petal.Length
+iris$Species
+
+dane<-iris[which(iris$Species=="virginica"),]
+head(dane)
+t.test(dane$Petal.Length,conf.level = 0.99)$conf.int
+platki<-dane$Petal.Length
+n<-length(platki)
+
+alpha<-0.05
+przedzial_war<-c((n-1)*var(platki)/qchisq(1-alpha/2,n-1),
+                 (n-1)*var(platki)/qchisq(alpha/2,n-1))
+sqrt(przedzial_war)
+
+
+#----zad 3.9-------
+
+binom.test(x=3,n=12)$conf.int
+
+#test hipotezy
+binom.test(x=3,n=12, p=1/6)
